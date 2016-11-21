b769bcbf1f4406063fc73e864677e82a6e4a01a8
kevguy <kevlai22@uw.edu>
2016-11-22 02:05:32 +0800

    feat: add node server support

---
+++ Framework/server.js
@@ -0,0 +1,10 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+var port = process.env.PORT || 5000;
+
+
+connect().use(serveStatic(__dirname)).listen(port, function(){
+    console.log('Server running on ' + port + '...');
+});
+
